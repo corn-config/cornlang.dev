6a1208cce7662b56db5ff9699e32798921830cbe
Jake Stanger <mail@jstanger.dev>
2023-08-31 23:17:03 +0100

    build: remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import topLevelAwait from "vite-plugin-top-level-await";
 import cornLanguage from "./cornLanguage.ts";
 import mdx from "@astrojs/mdx";
 
-import vercel from "@astrojs/vercel/static";
-
 // https://astro.build/config
 export default defineConfig({
   markdown: {
@@ -17,7 +15,5 @@ export default defineConfig({
   integrations: [mdx()],
   vite: {
     plugins: [wasm(), topLevelAwait()]
-  },
-  output: "server",
-  adapter: vercel()
+  }
 });
